793e29767799572b2faf80c79bcd57615be408eb
Gunnar Hakonarson <gunnarh@Gunnars-MacBook-Air.local>
2014-11-01 20:02:50 +0000

    Initial Commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Gunnar Hakonarson on 01/11/2014.
+//  Copyright (c) 2014 Gunnar Hakonarson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
