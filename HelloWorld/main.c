dea52c0bfbcca2f708b73fc96f5c93bfd054258f
ghakonarson <gunnar@gunnar.me.uk>
2016-01-01 20:59:42 +0000

    Test
    Just a test

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -7,9 +7,45 @@
 //
 
 #include <stdio.h>
+#include <stdbool.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int     main()
+{
+    int     vFirstArg,
+    vSecondArg;
+    char    vOperation;
+    bool    vFinished;
+    
+    // Make sure our flag is initialized!
+    vFinished = false;
+    
+    // Now loop until user doesn't want anymore:
+    while( vFinished != true )
+    {
+        printf( "What operation do you want to do?\n" );
+        scanf( "%c", &vOperation );
+        fpurge( stdin );
+        
+        if( vOperation == '+' )
+        {
+            printf( "Enter left argument: " );
+            scanf( "%d", &vFirstArg );
+            fpurge( stdin );
+            
+            printf( "\nEnter right argument: " );
+            scanf( "%d", &vSecondArg );
+            fpurge( stdin );
+            
+            printf( "\n%d + %d = %d\n",
+                   vFirstArg,
+                   vSecondArg,
+                   vFirstArg + vSecondArg );
+        }
+        else
+            vFinished = true;
+    }
+    
+    printf( "Finished.\n" );
+    
     return 0;
 }
